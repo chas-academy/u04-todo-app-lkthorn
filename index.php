b3b2d5e8e52fd1346298f10bc8ebaa8401577c5c
Lícia Thörn <licia.thorn@gmail.com>
2022-01-25 15:56:59 +0100

    start form in file index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List</title>
+</head>
+
+<body>
+    <div class="wrapper">
+    <form action="" method="POST">
+        <div class="group">
+            <label>To Do</label>
+            <input type="text" name="todo" value="Enter your next reminder">
+        </div>
+        <div class="group">
+            <button type="submit" name="submit">Submit</button>
+        </div>
+
+
+    </form>
+    </div>
+</body>
+
+</html>
