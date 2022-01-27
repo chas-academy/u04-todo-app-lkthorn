e67f8eb58a92dcdd8b6aeb8f6715a8bd805fd0d2
Lícia Thörn <licia.thorn@gmail.com>
2022-01-27 12:06:01 +0100

    add db.php and process.php to project

---
+++ app/public/db.php
@@ -0,0 +1,11 @@
+<?php 
+try {
+    $db = new PDO('mysql:host=mysql;dbname=tutorial','tutorial','secret');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    echo 'Connectet to database';
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+
+
+}
