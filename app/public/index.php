48e5e309308d1c9b6e121916af159741d1279371
Lícia Thörn <licia.thorn@gmail.com>
2022-01-27 12:04:07 +0100

    add create.php file as part of CRUD and update index.php

--- app/public/index.php
+++ app/public/index.php
@@ -11,20 +11,53 @@
 </head>
 
 <body>
+    <?php require 'db.php'; ?>
         <div class="wrapper">
-        <form action="process.php" method="POST">
-            <div class="group">
-                <label>To Do</label>
-                <input type="text" name="todo" value="Enter your next reminder">
-            </div>
-            <div class="group">
-                <button type="submit" name="submit">Submit</button>
+            <div class="add-section">
+                <form action="create.php" method="POST">
+                    <?php if (isset($_GET['mess']) && $_GET['mess'] == 'error'){ ?>
+                 
+                    <label>To Do</label>
+                    <input type="text" name="todo" value="Mandatory field">
+                    <button type="submit" name="submit">Submit</button>
+
+                                      
+                    <?php } 
+                    else { ?>
+                    <input type="text" name="todo" value="Enter a reminder" />
+                    <button type="submit" name="submit">Submit</button>
+                    <?php } ?>
+                </form> 
             </div>
 
-        </form> 
+            <?php 
+                $todos = $db->query("SELECT * FROM todo ORDER BY id"); 
+            ?>
+            <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) {?>
+                <div class="todo-item">
+                    <span id="<?php echo $todo['id']; ?>"
+                        class="delete-to-do">x</span>
+                    <?php if($todo['checked']){ ?>
+                    <input type="checkbox" class="check-box" data-todo-id ="<?php echo $todo['id']; ?>" checked />
+                    <h2 class="checked"> <?php echo $todo['title'] ?></h2>
+
+                    <?php } else {?>
+                        <input type="checkbox"
+                        data-todo-id ="<?php echo $todo['id']; ?>" class="check-box" />
+                        <h2><?php echo $todo['title'] ?></h2>
+                        <?php } ?>
+                        
+                </div>
+
+            <?php } ?> 
+
+
+
+
+     
       
 
-    </div>
+        </div>
 </body>
 
 </html>
